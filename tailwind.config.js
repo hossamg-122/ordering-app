6bad97a4f8db23576e75672a65876d241bc6c08e
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-05-21 09:09:05 +0200

    style: installing foodics fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {}
+    colors:{
+      header:'#F2F2F2'
+    },
+    fontFamily:{
+      aHelvetica:"Arabic Helvetica"
+    },
+    extend: {
+      fontFamily:{
+        aHelvetica:"Arabic Helvetica"
+      }
+    }
   },
   variants: {
-    extend: {}
+    extend: {
+    
+    }
   },
   plugins: []
 }
