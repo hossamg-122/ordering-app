e5b9f9a8252d48727b0749a33541e3ba405a2f2c
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-05-22 11:27:45 +0200

    feat: handling home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld class="text-red-800" msg="Welcome to Your Vue.js App" />
+  <div class="bg-gray-200">
+    <div class="container mx-auto">
+      <div
+        class="grid grid-cols-1 gap-x-0 gap-y-4 sm:grid-cols-4 sm:gap-x-4 lg:grid-cols-4 xl:gap-x-4"
+      >
+        <div class="py-16 sm:py-18 order-1 sm:order-none">
+          <!-- this is a generic component responsible for handling selected  products and sending them to the API and it takes the selected products with the power of event Bus -->
+          <OrderSummary />
+        </div>
+        <div class="col-span-3 self-start py-16 px-4 sm:py-18 sm:px-6 lg:px-8">
+          <!-- this component is responsible for rendering the products from the api -->
+          <ProductsList />
+        </div>
+      </div>
+      <!-- this is a generic component responsible for handling selecting a product and taking the selected product with the power of event Bus -->
+      <ProductModal />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ProductsList from "@/components/products-list.vue";
+import OrderSummary from "../components/order-summary.vue";
+import ProductModal from "@/components/product-modal.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: { ProductsList, OrderSummary, ProductModal },
 };
 </script>
