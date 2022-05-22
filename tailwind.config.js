ab4efc4b7a965b06b11e7a0b759fd80ee455b427
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-05-22 11:28:09 +0200

    style: customizing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,21 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    colors:{
-      header:'#F2F2F2'
-    },
+  
     fontFamily:{
       aHelvetica:"Arabic Helvetica"
     },
     extend: {
+      colors:{
+        primary:"#5830C5",
+        primaryHovered:"#312490",
+        header:'#F2F2F2'
+      },
       fontFamily:{
         aHelvetica:"Arabic Helvetica"
+      },
+      fontSize:{
+        'xx':'.5rem'
       }
     }
   },
