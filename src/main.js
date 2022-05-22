c8800a4c73c274bf4fe6ea7243b26b0d323a5807
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-05-22 11:25:31 +0200

    feat: adding event bus and filter

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import router from "./router";
 import "./index.css";
 
 Vue.config.productionTip = false;
-
+// creating an event bus to share data between components
+export const eventBus = new Vue();
+// this filter is responsible for handling currency displaying
+Vue.filter("currency", value => {
+  return  value +" "+ "SAR";
+});
 new Vue({
   router,
   render: (h) => h(App),
